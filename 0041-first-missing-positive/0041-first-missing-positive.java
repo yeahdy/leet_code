bc54d4fe776bd2ef7f35a9d172bd60b2bc0539a5
이예진 <86579541+yeahdy@users.noreply.github.com>
2022-11-19 16:04:17 +0900

    Time: 105 ms (5.02%), Space: 70.7 MB (5.09%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int result = 0;
+    	Arrays.sort(nums);
+    	List<Integer> list = new ArrayList<>();
+    	
+    	for(int num : nums) {
+    		if(num > 0) {
+    			list.add(num);
+    		}
+    	}
+    	    	
+    	// 누락된 최소 정수 n = 배열의 최소정수 < n < 배열의 최대정수
+    	int j = 0;
+        if(list.size() > 0){
+            list = list.stream().distinct().collect(Collectors.toList());
+            for(int i= 1; i < list.get(list.size()-1); i++) {
+    		    if(list.get(j) != i) {
+    			    result = i;
+    			    break;
+    		    }
+    		    j++;
+    	    }
+        }
+    	
+    	if(result == 0) {
+            if(list.size() > 0){
+                result = list.get(list.size()-1) + 1; 
+            }else {
+                result = 1;
+            }
+    		
+    	}
+    	
+    	return result;
+    }
+}
