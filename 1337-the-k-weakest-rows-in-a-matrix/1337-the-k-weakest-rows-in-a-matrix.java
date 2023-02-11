9f213eb44005c1ece4ae4a97f7cbb9680bcfbb73
이예진 <86579541+yeahdy@users.noreply.github.com>
2023-02-11 17:24:27 +0900

    Time: 4 ms (33.48%), Space: 43.2 MB (91.77%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        Map<Integer,Integer> hashMap = new TreeMap<>();
+        Map<Integer,Integer> hashMap = new HashMap<>();
 		
 		// 각 행에서 1을 갯수를 count
 		for(int i=0; i < mat.length; i++) {
@@ -24,5 +24,13 @@ class Solution {
 		}
 		
         return result;
+        //        다른사람 답 
+//        return map.entrySet().stream()
+//                .sorted(Map.Entry.comparingByValue())
+//                .limit(k)	
+//                .map(Map.Entry::getKey)	// 연산한 새로운 stream 반환
+//                .mapToInt(it -> it)		// IntStream 타입으로 반환.
+                                            // int[] 로 변환을 위해 Integer > int 변환 필요 
+//                .toArray();
     }
 }
