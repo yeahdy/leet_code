d16efcc95e8bb34acacc8ee94b2c0da1882592db
이예진 <86579541+yeahdy@users.noreply.github.com>
2023-04-08 14:37:51 +0900

    Time: 445 ms (5.81%), Space: 44.6 MB (16.04%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,12 +1,32 @@
 class Solution {
-    public int[] sortedSquares(int[] nums) {
-        int i= 0;
-        int[] result = new int[nums.length];
-        for(int num : nums){
-            result[i] = (int)Math.pow(num, 2);
-            i++;
+    public int[] sortedSquares(int[] arr) {
+        // int i= 0;
+        // int[] result = new int[nums.length];
+        // for(int num : nums){
+        //     result[i] = (int)Math.pow(num, 2);
+        //     i++;
+        // }
+        // Arrays.sort(result);
+        // return result;
+        
+        // Point! 두 개의 포인터를 이용하여 동시에 비교하기 때문에 빠르게 정렬이 가능하다.
+        int L = 0, R = arr.length-1;
+        int nums[] = new int[arr.length];
+        for(int i = nums.length-1; i>=0; i--){
+            if(Math.abs(arr[L])>=Math.abs(arr[R])){
+                System.out.println("Left :: "+arr[L]);
+                nums[i]=arr[L]*arr[L];
+                L++;
+            }
+            else{
+                System.out.println("Right :: "+arr[R]);
+                nums[i] =arr[R]*arr[R];
+                R--;
+            }
+            System.out.println("nums[i] :: "+ nums[i]);
         }
-        Arrays.sort(result);
-        return result;
+        return nums;
+        
     }
+    
 }
